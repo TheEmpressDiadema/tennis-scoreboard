3ed26c68bfb4433e059508bbacd0b5e2d29a658a
TheEmpressDiadema <mauhkpaffftep@gmail.com>
2026-09-17 21:30:37 +0300

    initial commit

---
+++ src/tennis_scoreboard/__init__.py
@@ -0,0 +1,12 @@
+from waitress import serve
+
+def wsgiapp(environ, start_response):
+    status = '200 OK'
+    headers = [('Content-type', 'text/plain; charset=utf-8')]
+
+    start_response(status, headers)
+
+    return [b'Hello from pure Python WSGI']
+
+def main() -> None:
+    serve(wsgiapp, listen='*:8080')
